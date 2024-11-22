624352e73ac491311ee6cd5e943bebbce63b3ef2
platinumscatter <platinumscatter@gmail.com>
2024-11-22 11:52:05 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "github.com/gorilla/mux"
+)
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Hello, World!")
+}
+
+func main() {
+    router := mux.NewRouter()
+    router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+    http.ListenAndServe(":8080", router)
+}
