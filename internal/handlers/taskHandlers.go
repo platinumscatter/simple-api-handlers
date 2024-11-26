f224c31714e1d721fdc59e925f506ff334ec60fb
platinumscatter <platinumscatter@gmail.com>
2024-11-26 14:18:54 +0300

    Apply Repository pattern

---
+++ internal/handlers/taskHandlers.go
@@ -0,0 +1,74 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/platinumscatter/simple_api/internal/taskService"
+)
+
+type Handler struct {
+	Service *taskService.TaskService
+}
+
+func NewHandler(service *taskService.TaskService) *Handler {
+	return &Handler{
+		Service: service,
+	}
+}
+
+func (h *Handler) GetTaskHandler(w http.ResponseWriter, r *http.Request) {
+	tasks, err := h.Service.GetAllTasks()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func (h *Handler) PostTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task taskService.Task
+	err := json.NewDecoder(r.Body).Decode(&task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	createdTask, err := h.Service.CreateTask(task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(createdTask)
+}
+
+func (h *Handler) UpdateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task taskService.Task
+	err := json.NewDecoder(r.Body).Decode(&task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	updatedTask, err := h.Service.UpdateTaskByID(task.ID, task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(updatedTask)
+}
+
+func (h *Handler) DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task taskService.Task
+	err := json.NewDecoder(r.Body).Decode(&task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	err = h.Service.DeleteTaskByID(task.ID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
---
+++ internal/taskService/repository.go
@@ -0,0 +1,42 @@
+package taskService
+
+import "gorm.io/gorm"
+
+type TaskRepository interface {
+	CreateTask(task Task) (Task, error)
+	GetAllTasks() ([]Task, error)
+	UpdateTaskByID(id uint, task Task) (Task, error)
+	DeleteTaskByID(id uint) error
+}
+
+type taskRepository struct {
+	db *gorm.DB
+}
+
+func NewTaskRepository(db *gorm.DB) *taskRepository {
+	return &taskRepository{db: db}
+}
+
+func (r *taskRepository) CreateTask(task Task) (Task, error) {
+	result := r.db.Create(&task)
+	if result.Error != nil {
+		return Task{}, result.Error
+	}
+	return task, nil
+}
+
+func (r *taskRepository) GetAllTasks() ([]Task, error) {
+	var tasks []Task
+	err := r.db.Find(&tasks).Error
+	return tasks, err
+}
+
+func (r *taskRepository) UpdateTaskByID(id uint, task Task) (Task, error) {
+	err := r.db.Model(&task).Where("id = ?", id).Updates(task).Error
+	return task, err
+}
+
+func (r *taskRepository) DeleteTaskByID(id uint) error {
+	err := r.db.Delete(&Task{}, id).Error
+	return err
+}
